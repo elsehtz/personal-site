5953664442b091ca93d015831e73804f8df19d60
Zak ElSeht <elseht17@gmail.com>
2026-06-11 01:16:29 -0400

    adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: "hybrid",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  output: "static",
+  adapter: vercel(),
   integrations: [react()],
 });
